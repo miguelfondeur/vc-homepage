986f00d9ca8132868c9967d0c1b943a7af6191c7
Miguel fondeur <miguel@mfondeur.lan>
2025-05-23 16:42:53 -0400

    add all of the things

--- .eleventy.js
+++ .eleventy.js
@@ -1,44 +1,95 @@
+const fs = require("fs");
 const esbuild = require("esbuild");
 
-// CSS bundling function
+// Bundles your CSS
 async function bundleCss() {
-  const result = await esbuild.build({
-      entryPoints: ["src/css/main.css"],
-      bundle: true,
-      minify: true,
-      outfile: "public/css/main.min.css",
-      loader: {
-          ".css": "css"
-      }
-  });
-  return result;
+    await esbuild.build({
+        entryPoints: ["src/css/main.css"],
+        bundle: true,
+        minify: true,
+        outfile: "public/css/main.min.css",
+        loader: {
+            ".css": "css",
+            ".png": "file",
+            ".jpg": "file",
+            ".svg": "file",
+            ".gif": "file",
+            ".webp": "file"
+        },
+        assetNames: "assets/img/[name]",  // Cache-busting hash filenames
+    });
+}
+
+// Bundles your JS code
+async function bundleJs() {
+    await esbuild.build({
+        entryPoints: ["src/js/index.js"],      
+        bundle: true,
+        minify: true,
+        outfile: "public/js/bundle.js",       // Bundled output
+        format: "esm",                        // ES Module format
+        platform: "browser",                  // Targeting browser
+        sourcemap: true,                      // Source maps for debugging
+        loader: {
+            ".js": "js"
+        }
+    });
+}
+
+// Image processing shortcode for HTML
+async function imageShortcode(src, alt, className = "", widths = [300, 600, 1200], formats = ["webp", "jpeg"]) {
 }
 
 module.exports = function(eleventyConfig) {
+    // Add absoluteUrl filter - using relative URLs for local development
+    eleventyConfig.addFilter("absoluteUrl", function(url) {
+        if (url.startsWith("http")) {
+            return url;
+        }
+        return url.startsWith("/") ? url : `/${url}`;
+    });
 
-  // CSS
-  eleventyConfig.addPassthroughCopy("src/css");
-  eleventyConfig.addWatchTarget("src/css");
+    // Passthroughs
+    eleventyConfig.addPassthroughCopy({ "src/netlify": "netlify" });
+    eleventyConfig.addPassthroughCopy({ "src/manifest.json": "" });
+    eleventyConfig.addPassthroughCopy({ "src/pdfs": "pdfs" });
+    eleventyConfig.addPassthroughCopy({ "src/models": "models" });   // GLTF model passthrough
+    eleventyConfig.addPassthroughCopy({ "src/js": "js" });
+    eleventyConfig.addPassthroughCopy("src/css");
+    eleventyConfig.addPassthroughCopy({ "src/assets": "assets" });   // Copy assets directory
+    eleventyConfig.addPassthroughCopy({ "src/img": "img" });         // Copy img directory
+    eleventyConfig.addWatchTarget("src/css");
+    eleventyConfig.addWatchTarget("src/assets");  // Watch assets directory
+    eleventyConfig.addWatchTarget("src/img");     // Watch img directory
+    eleventyConfig.addWatchTarget("src/articles"); // Watch articles directory
 
-  eleventyConfig.addPassthroughCopy("src/images");
+    // Collections
+    eleventyConfig.addCollection("articles", function(collection) {
+        return collection.getFilteredByGlob("src/articles/**/*.html");
+    });
 
-  // JS
-  eleventyConfig.addPassthroughCopy("src/js");
-  eleventyConfig.addWatchTarget("src/js");
+    // Image shortcode for HTML
+    eleventyConfig.addNunjucksAsyncShortcode("image", imageShortcode);
+    eleventyConfig.addLiquidShortcode("image", imageShortcode);
 
-  // Fonts
-  eleventyConfig.addPassthroughCopy("src/fonts");
-  
-  // Copy minified CSS from public to _site
-  eleventyConfig.addPassthroughCopy({ "public/css/main.min.css": "css/main.min.css" });
+    // CSS & JS bundling on Eleventy build
+    eleventyConfig.on("eleventy.before", async () => {
+        await bundleCss();
+        await bundleJs();
+    });
 
-  return {
-    dir: {
-      input: "src",
-      output: "_site",
-      includes: "_includes",
-      llayouts: "_includes", 
-      data: "_data"
-    }
-  };
-}; 
+    // Read file filter
+    eleventyConfig.addFilter("readFile", function (filepath) {
+        return fs.readFileSync(filepath, "utf8");
+    });
+
+    return {
+        dir: {
+            input: "src",
+            data: "_data",
+            output: "public",
+            includes: "_includes",
+            layouts: "_includes"
+        }
+    };
+};
---
+++ public/js/bundle.js
@@ -0,0 +1,2 @@
+function t(){document.addEventListener("click",e=>{if(!e.target.closest("#theme-toggle"))return;let o=document.documentElement.dataset.theme==="dark"?"light":"dark";document.documentElement.dataset.theme=o,localStorage.setItem("theme",o)})}console.log("%cWelcome to MiguelFondeur.com","color: #b6a572; font-size: 18px; font-family: serif;");t();
+//# sourceMappingURL=bundle.js.map
